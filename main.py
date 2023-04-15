9fa58f99b3cfd2d52a90012b29cb8aaba4872cd2
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-15 17:29:47 +0000

    Add main.py for submission without LighningCLI usage

---
+++ main.py
@@ -0,0 +1,44 @@
+import importlib
+import yaml
+
+
+CONFIG_PATH = 'run/configs/fake_config.yaml'
+
+
+def get_class(class_path):
+    module_path = '.'.join(class_path.split('.')[:-1])
+    class_name = class_path.split('.')[-1]
+    return getattr(importlib.import_module(module_path), class_name)
+
+
+def main():
+    # Get classes
+    with open(CONFIG_PATH, 'r') as f:
+        config = yaml.safe_load(f)
+    
+    ModelClass = get_class(config['model']['class_path'])
+    DatamoduleClass = get_class(config['data']['class_path'])
+    PredictionWriterClass = get_class(config['trainer']['callbacks'][0]['class_path'])
+
+    # Load model
+    model = ModelClass.load_from_checkpoint('final.ckpt')
+
+    # Load data
+    datamodule = DatamoduleClass()
+    datamodule.setup('test')
+    test_dataloader = datamodule.test_dataloader()
+
+    # Load prediction writer
+    prediction_writer = PredictionWriterClass('submission.csv')
+
+    # Predict 
+    predictions = []
+    for batch in test_dataloader:
+        predictions.extend(model(batch))
+
+    # Write predictions
+    prediction_writer.write_on_epoch_end(None, None, predictions, None)
+
+
+if __name__ == '__main__':
+    main()
