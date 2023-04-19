60d935025665645f15f2438222768474b82fb10f
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-19 15:17:58 +0000

    Add config overwrite back to fix wandb disabled case

--- run/main.py
+++ run/main.py
@@ -2,7 +2,13 @@ from src.utils.utils import MyLightningCLI, TrainerWandb
 
 
 def cli_main():
-    cli = MyLightningCLI(trainer_class=TrainerWandb, save_config_kwargs={'config_filename': 'config_pl.yaml'})
+    cli = MyLightningCLI(
+        trainer_class=TrainerWandb, 
+        save_config_kwargs={
+            'config_filename': 'config_pl.yaml',
+            'overwrite': True,
+        }
+    )
 
 
 if __name__ == "__main__":
