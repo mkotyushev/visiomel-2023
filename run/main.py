60df6268d6351f16de9ab054e5d1fa62e8fcc30f
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-19 15:03:45 +0000

    Fix config overriden by providing different filename

--- run/main.py
+++ run/main.py
@@ -2,8 +2,7 @@ from src.utils.utils import MyLightningCLI, TrainerWandb
 
 
 def cli_main():
-    # TODO: check why configs is overriden when wandb is online
-    cli = MyLightningCLI(trainer_class=TrainerWandb, save_config_kwargs={"overwrite": True})
+    cli = MyLightningCLI(trainer_class=TrainerWandb, save_config_kwargs={'config_filename': 'config_pl.yaml'})
 
 
 if __name__ == "__main__":
