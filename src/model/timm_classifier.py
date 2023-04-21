c7098b91468b248ec4ac844ad825e20b1f521f4e
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-21 12:23:46 +0000

    Add timm classifier

---
+++ src/model/timm_classifier.py
@@ -0,0 +1,44 @@
+import timm
+import torch
+from typing import Any, Dict, Optional
+
+from src.model.visiomel_model import VisiomelModel
+
+
+class TimmClassifier(VisiomelModel):
+    def __init__(
+        self, 
+        backbone_name, 
+        num_classes: int = 2, 
+        optimizer_init: Optional[Dict[str, Any]] = None,
+        lr_scheduler_init: Optional[Dict[str, Any]] = None,
+        pl_lrs_cfg: Optional[Dict[str, Any]] = None,
+        pretrained: bool = True,
+        finetuning: Optional[Dict[str, Any]] = None,
+        log_norm_verbose: bool = False,
+        lr_layer_decay: float = 1.0,
+    ):
+        super().__init__(
+            optimizer_init=optimizer_init, 
+            lr_scheduler_init=lr_scheduler_init,
+            pl_lrs_cfg=pl_lrs_cfg,
+            finetuning=finetuning, 
+            log_norm_verbose=log_norm_verbose,
+            lr_layer_decay=lr_layer_decay,
+        )
+        self.save_hyperparameters()
+
+        self.classifier = timm.create_model(backbone_name, pretrained=pretrained, num_classes=num_classes)
+
+        # TODO: called in each VisiomelModel subclass but after subclass __init__
+        # need to move to VisiomelModel somehow
+        self.unfreeze_only_selected()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+    
+    def compute_loss_preds(self, batch, *args, **kwargs):
+        x, y = batch
+        preds = self(x)
+        loss = self.loss_fn(preds, y)
+        return loss, {'ce': loss}, preds
