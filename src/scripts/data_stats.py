be9b03f5db9b8360a6e917fb9d9e2286576a194e
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-22 15:00:01 +0000

    Add colors calculation

--- src/scripts/data_stats.py
+++ src/scripts/data_stats.py
@@ -6,20 +6,25 @@
 import argparse
 import numpy as np
 import imagesize
-import re
+import cv2
 from pathlib import Path
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input-dir', type=Path, help='Path to data folder')
+parser.add_argument('--in-image-stats', action='store_true', help='If true, calculate in image stats. Takes longer time.')
 args = parser.parse_args()
 
 # Read data image by image and save shape to array
 filepaths = sorted(list(args.input_dir.glob('**/*.png')))
 
-shapes = []
+shapes, colors = [], []
 for filepath in tqdm(filepaths):
     w, h = imagesize.get(filepath)
+    if args.in_image_stats:
+        # Read image and calculate in image stats
+        img = cv2.imread(str(filepath))
+        colors.append(img.mean(0).mean(0))
     shapes.append((w, h))
 
 # Calcualte mean for each dimention
@@ -28,6 +33,10 @@ print('Mean: ', shapes.mean(0))
 print('Min: ', shapes.min(0))
 print('Max: ', shapes.max(0))
 print('Median: ', np.quantile(shapes, 0.5, 0))
+
+colors = np.array(colors)
+print('Mean: ', colors.mean(0))
+print('Std: ', colors.std(0))
 
 # Mean:
 # 7: [ 836.26304024 1027.62816692]
