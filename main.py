d30581dcf352d200f71d6fc90c7180005531443d
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-05-09 13:36:58 +0000

    Add path modifications

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
 import argparse
 import pandas as pd
 import timm
